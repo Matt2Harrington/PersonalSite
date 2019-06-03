b6558d1bcb181588874c166b1c4c2a8f898cb233
Matt Harrington <mharr1729@gmail.com>
2019-06-02 23:33:30 -0400

    Matt/work on site (#15)
    * fixed overflow issues
    
    * more work completed on the text formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,14 +17,16 @@ $(document).ready(function() {
 
 });
 
-
-
-
-  document.getElementById("fmuInfo").innerHTML = "Bachelor's of Science in Computer Science with a minor in Mathematics from Francis Marion University. Located in Florence, South Carolina.";
+  // adding text to the education section
+  document.getElementById("fmuInfo").innerHTML = "B.S. in Computer Science, with a minor in Mathematics, from Francis Marion University. Located in Florence, South Carolina.";
+  document.getElementById("computerScienceHonorGraduate").innerHTML = "Computer Science Honorary Graduate for the Class of 2019.";
+  document.getElementById("presidentList").innerHTML = "Graduated with a cumulative 4.0 GPA, making the President's List each semester at Francis Marion University.";
+  document.getElementById("upsilonPiEpsilon").innerHTML = "President of the Upsilon Pi Epsilon Computer Science Honor Society from 2018 to 2019.";
+  document.getElementById("phiKappaPhi").innerHTML = "Inducted into Phi Kappa Phi Honor Society.";
 
   // puts an image into an id.
   var fmuLogo = new Image(5,5);
-  fmuLogo.src = "assets/images/White M2H.png";
+  fmuLogo.src = "assets/images/cropped-Site-Icon-Full-Color-e1502396334483 white.png";
   $("#fmuLogo").attr("src", fmuLogo.src);
 
 });
