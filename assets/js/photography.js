6647005386566374d77adf18353a03e4175dc3a1
Matt Harrington <matt2harrington@gmail.com>
2019-10-22 23:10:22 -0400

    added footer with links and starting password process

--- assets/js/photography.js
+++ assets/js/photography.js
@@ -65,5 +65,78 @@ $('a[href*="#"]')
 });
 
 
+// function createModal(ID, title, forceStay, modalBody, cancel, submitBtn) {
+//     // create the modal html in string representation
+//     var modal = '<div class="modal fade" id="' + ID + '" tabindex="-1" role="dialog" aria-labelledby="Clinic ID" aria-hidden="false" data-keyboard="false">';
+//     if (forceStay) {
+//       modal = '<div class="modal fade" id="' + ID + '" tabindex="-1" role="dialog" aria-labelledby="Clinic ID" aria-hidden="false" data-backdrop="static" data-keyboard="false" >';
+//     }
+//     modal += '<div class="modal-dialog modal-dialog-centered" role="document">';
+//     modal += '<div class="modal-content">';
+//     modal += '<div class="modal-header">';
+//     modal += '<h5 class="modal-title">' + title + '</h5>';
+//     modal += '</div>';
+//     modal += '<div class="modal-body">';
+//     modal += modalBody;
+//     modal += '</div>';
+//     modal += '<div class="modal-footer">';
+//     // add the cancel button if it is wanted
+//     if (cancel) {
+//       modal += '<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>';
+//     }
+//     modal += submitBtn;
+//     modal += '</div></div></div></div>';
+//     // makes the modal appear by converting the strings above into HTML
+//     document.body.insertAdjacentHTML('beforeend', modal);
+//     // handles if there are any page size changes
+//     $('#' + ID).modal('handleUpdate');
+//   }
+//
+//   function openRemoveModal() {
+//     var removeClinicModalBtn = '<button type="button" id="removeClinicButtonFinal" class="btn btn-danger" data-dismiss="modal">Remove</button>';
+//     var removeClinicBody = '<div class="form-group"><input id="clinicInput" type="text" class="form-control" name="clientIDBox" placeholder="Clinic ID" value=""></div>';
+//     createModal('logInModal', 'Log In', true,
+//       "Please Enter Your Password" + removeClinicBody, true, removeClinicModalBtn); // create reset modal for future use
+//     // makes the modal open
+//     $('#logInModal').modal({
+//       keyboard: false,
+//       backdrop: 'static'
+//     });
+//
+//   document.getElementById('clinicInput').value = "";
+//
+//   // this function deletes the individual clinic by hash
+//   function deleteClinic() {
+//
+//     var clinicID = getInputVal('clinicInput');
+//     var docRef = db.collection("Locations").doc(clinicID);
+//
+//     docRef.get().then(function(doc) {
+//       if (doc.exists) {
+//         db.collection("Locations").doc(clinicID).delete().then(function() {
+//             console.log("Document successfully deleted!");
+//             successAlert(clinicID);
+//         }).catch(function(error) {
+//             console.error("Error removing document: ", error);
+//         });
+//       } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//         errorAlert("No Such Document");
+//       }
+//
+//     }).catch(function(error) {
+//       console.log("Error getting document:", error);
+//       errorAlert("Please Enter A Clinic ID.")
+//     });
+//
+//   } // end of deleteClinic method
+//
+//     var removeBtnModal = document.getElementById('removeClinicButtonFinal');
+//     removeBtnModal.addEventListener('click', deleteClinic);
+//   }
+//   // this opens the Remove Clinic Modal
+//   document.getElementById("addImage").addEventListener('click', openRemoveModal);
 
-});
+
+}); // ready
