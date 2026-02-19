324bfd486e95cf563eb487c22e0bd9a3d4146aef
Matt Harrington <mharr1729@gmail.com>
2026-02-19 00:01:52 -0500

    added privacy policy pages and shortened text on about you

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,18 +1,15 @@
 export const aboutParagraphs = [
-  "Hello, my name is Matt Harrington, and I am Senior iOS Software Engineer Consultant. First introduced to iOS development in 2009, I was in complete awe as a kid, wondering how to create such cool things.",
+  "Hello, my name is Matt Harrington, and I am Senior iOS Software Engineer Consultant.",
   "Over the years my passion of mathematics and science lead me to pursure a field in engineering. Since I always had a piece of tech in my hands, it lead to me to pursue a Computer Science degree.",
-  "Since making that declaration, I was driven to give college my absolute 110% and ended up with a 4.0 GPA, graduating in 2019. Since then, I have been lucky enough to have worked on many different problems, with many different outcomes that have lead me far into the iOS and tech space, exactly where I dreamed of being back in my earlier years!",
+  "I graduated in 2019 with a 4.0 GPA with my major in CS, minor in Mathematics. Since then, I have been lucky enough to have worked on many different problems, with many different outcomes that have lead me far into the iOS and tech space, exactly where I dreamed of being back in my earlier years!",
 ];
 
 export const myProgressParagraphs = [
-  "My first introduction into Computer Science was learning basic HTML, CSS, and JavaScript in 2013. I took a web design course in High School that made me fall in love with the computer in a way I hadn't before.",
-  "As I began college in 2015, I was introduced to Java in my basic coding course. I had no idea at the time, but the next four years would be my most stressful, but also my most rewarding. I grew to love social environments, taking leadership positions whenever possible. This only built upon the natural business abilities that I have accumulated since childhood.",
-  "Since being in the technology field in 2019, there has been so much change and updates, but I view these changes as opportunity to improve myself, to learn and grow. Growing with iOS Devlopment since the introduction of SwiftUI in 2019, as well as many other new tools and frameworks!",
-  "Throughout the years I have worked on features that over 20 million users have interacted with, including a new authentication method for an enterprise banking app wrapped in a custom, native SDK that I have helped create since 2022."
+  "First introduced to iOS development in 2009 while watching WWDC, I was in complete awe as a kid, wondering how to create such cool things.",
+  "During my time working in a professional environment, I have worked on features that over 20 million users have used, with some of the latest frameworks and ideologies such as SwiftUI, Async-Await, and Modular UI approaches through custom built SDKs!"
 ];
 
 export const myHobbiesParagraphs = [
-  "My love for music goes back to childhood, but it grew after I learned how to play bass guitar. I am self-taught and teaching myself how to play bass was another stepping stone in how to self-learn. Over the past six years, my experience of computer science only helped me see the similarities between code and music.",
-  "My passion for health began in 2014 after a lifetime of weight issues. The devotion to my health has transcended my lifestyle in ways I would never be able to imagine years ago. Workouts, a steady diet of grilled chicken and salads, and the reminder that the brain benefits just as much as the body helped me through the most stressful situations in college. Since I began in 2013, I have lost 150lbs. Each day is a new battle.",
-  "For many years photography has been my favorite hobby. Years ago, I even created a small business with it for myself. I am self-taught, but it comes from my grandfather. He loved photography while I was growing up, and it translated over to a passion for life. He gave me my eye, and that is the most important part of photography. It's what comes from within."
+  "My love for music goes back to childhood, but it grew after I learned how to play bass guitar. I am self-taught and teaching myself how to play bass was another stepping stone in how to self-learn.",
+  "For many years photography has been my favorite hobby and I love my Canon R5 Mark II very much!"
 ];
